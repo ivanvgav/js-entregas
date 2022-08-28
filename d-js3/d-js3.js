5867699bfe30a89b588611ed9838b87fa336fccd
ivanvgav <61671242+ivanvgav@users.noreply.github.com>
2022-08-28 13:24:20 -0300

    Inicio desafio complementario 3

---
+++ d-js3/d-js3.js
@@ -0,0 +1,27 @@
+// Loops o ciclos
+// Favor de descomentar las partes del código para probar cada uno de los algoritmos
+
+// Algoritmo para encontrar el sucesor de un número 0 a 10
+/*
+let number = Number(prompt("Ingrese un número del 0 al 10. Se le sumará el sucesor hasta llegar a 10."))
+
+for(let i = 1; i <= 10 ; i++) {
+  let resultado = number + i
+  alert(`El sucesor es: ${resultado}`)
+}
+*/
+
+//Algoritmo para escribir una concatenación de strings
+/*
+let entrada = prompt("Ingrese un caracteres o ESC para salir")
+let texto = " "
+
+while (entrada != "ESC" && entrada != "esc") {
+  texto += entrada + " "
+  entrada = prompt("Ingrese un texto o ESC para interrumpir")
+}
+
+alert(texto)
+*/
+
+// Algoritmo de Hola
