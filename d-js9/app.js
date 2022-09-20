40a512833ba75a4993ff609031fcd367f360f08f
ivanvgav <61671242+ivanvgav@users.noreply.github.com>
2022-09-20 16:00:38 -0300

    Inicio de programacion

--- d-js9/app.js
+++ d-js9/app.js
@@ -1,1 +1,18 @@
 // Eventos
+
+const formulario = document.querySelector("form");
+
+const nameForm = document.querySelector("#formuNombre");
+const mailForm = document.querySelector("#formuMail");
+
+function checkForm(e) {
+  e.preventDefault();
+  let givenName = document.createElement("div");
+  givenName.innerHTML = `<p><strong>Usuario Dado: ${nameForm.value}</p></strong>`;
+  let givenMail = document.createElement("div");
+  givenMail.innerHTML = `<p><em>Mail Dado: ${mail.value}</p></em>`;
+  document.body.appendChild(givenName);
+  document.body.appendChild(givenMail);
+}
+
+formulario.addEventListener("submit", checkForm);
